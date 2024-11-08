c7bf562eeed7175551575c46354d157089a97da2
vinaypylearningproject <vinaykumart700@gmail.com>
2024-11-08 21:51:48 +0530

    commit

---
+++ Nov/ex_07112024_python_var_keywords/lab0013_max_min_functions.py
@@ -0,0 +1,16 @@
+#to find max and min between two numbers
+
+#3,4-> max-4
+result1=max(3,4)
+print(result1)
+
+result2=max(456,-1,456,7565,-5657,567)
+print(result2)
+
+result3=min(45,56,78,89)
+print(result3)
+
+result4=min(-56,67,-67,90)
+print(result4)
+
+print(result1,result2,result3,result4)
